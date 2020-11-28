02a808f27d1cce77cc65193b5be1c20bbb5bf228
Helio Santos <heliosantos99@gmail.com>
2020-11-28 12:17:29 +0000

    add status code validation to mite scripts

--- mite_scripts/journeys/say_hello_journey.py
+++ mite_scripts/journeys/say_hello_journey.py
@@ -1,4 +1,4 @@
-# from .utils import check_status_code, check_status_code_in_groups
+from .utils import check_status_code
 from mite_browser import browser_decorator
 from mite.exceptions import MiteError
 from asyncio import sleep
@@ -11,7 +11,7 @@ async def say_hello_journey(ctx):
             'http://host.docker.internal:5000/',
             headers={'Host': 'host.docker.internal:5000', 'Connection': 'keep-alive', 'Pragma': 'no-cache', 'Cache-Control': 'no-cache', 'DNT': '1', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Sec-Fetch-Site': 'none', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-User': '?1', 'Sec-Fetch-Dest': 'document', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'en-US,en;q=0.9,pt;q=0.8'},
                         )
-        # check_status_code(resp, 200)
+        check_status_code(resp, 200)
     await sleep(1)
 
     async with ctx.transaction("Submit name"):
@@ -20,6 +20,5 @@ async def say_hello_journey(ctx):
             headers={'Host': 'host.docker.internal:5000', 'Connection': 'keep-alive', 'Content-Length': '9', 'Cache-Control': 'max-age=0', 'Origin': 'http://host.docker.internal:5000', 'Upgrade-Insecure-Requests': '1', 'DNT': '1', 'Content-Type': 'application/x-www-form-urlencoded', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Sec-Fetch-Site': 'same-origin', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-User': '?1', 'Sec-Fetch-Dest': 'document', 'Referer': 'http://host.docker.internal:5000/', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'en-US,en;q=0.9,pt;q=0.8'},
             data='name=Alex'
             )
-        # check_status_code(resp, 200)
+        check_status_code(resp, 200)
     await sleep(1)
-
---
+++ mite_scripts/journeys/utils.py
@@ -0,0 +1,18 @@
+from mite.exceptions import MiteError
+
+def check_status_code(resp, expectedStatusCode):
+    """Raise an error if the response has an unexpected status code.
+
+
+    Args:
+        resp: the response to falidate
+        expectedStatusCode: the status code expected the response to have
+
+
+    NOTE: 
+    There's a call to this function on the scripts created using the HAR converter.
+    The source code to the funcion in not part of the scripts and is not in the mite repo.
+    I'm not sure if this is the best approach but failed requests will return a MiteError since the contex handles it differently than other expections.
+    """
+    if resp.status_code != expectedStatusCode:
+        raise MiteError(f"Invalid status code. Expected: {expectedStatusCode}, Actual: {resp.status_code} ")
